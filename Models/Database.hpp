bcbdcf58f30f4930ce07a8974a2fc74987358dae
mahdibohloul <mahdi_bohloul@yahoo.com>
2020-05-23 17:52:13 +0430

    database class is for construct relation between separate models was created

---
+++ Models/Database.hpp
@@ -0,0 +1,17 @@
+#ifndef _DATABASE_H
+#define _DATABASE_H
+
+
+
+class Database
+{
+public:
+    Database();
+    ~Database();
+
+public:
+    class Person;
+    class Hotel;
+    class Comment;
+};
+#endif
