e8ae28623fb27f4d900e3a5762313f45cb832ad7
mahdibohloul <mahdi_bohloul@yahoo.com>
2020-05-23 17:58:30 +0430

    standard room is a level of room that can exist in every hotel

---
+++ Models/StandardRoom.hpp
@@ -0,0 +1,17 @@
+#ifndef _STANDARDROOM_H
+#define _STANDARDROOM_H
+
+#include "Room.hpp"
+
+class StandardRoom : public Database::Hotel::Room
+{
+public:
+    StandardRoom(float price);
+    ~StandardRoom();
+    std::string generate_id();
+
+private:
+    static int current_id;
+};
+
+#endif
