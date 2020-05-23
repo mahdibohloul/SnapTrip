753462a3f4fc745888e471351eb2ea653deb0849
mahdibohloul <mahdi_bohloul@yahoo.com>
2020-05-23 17:49:45 +0430

    Const names was created

---
+++ ConstNames.hpp
@@ -0,0 +1,16 @@
+#ifndef _CONSTNAME_H
+#define _CONSTNAME_H
+
+#include <string>
+
+namespace ConstNames
+{
+    const bool Not_Booked = false;
+    const bool Booked = true;
+    const std::string Standard_Room_Prefix = "s";
+    const std::string Delux_Room_Prefix = "d";
+    const std::string Luxury_Room_Prefix = "l";
+    const std::string Premium_Room_Prefix = "p";
+}
+
+#endif
